1d32563d6a54c104c0d60728cfe61f553bcfbdbf
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 22:38:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,9 @@ def reverse_each_word(sentence)
   sentence_to_array.each { |n| new_array << n.reverse}
   new_array.join(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence_to_array = sentence.split(" ")
+  sentence_to_array.collect { |n| n.reverse}
+  sentence_to_array.join(" ")
+end
